e89af9f4d965916458518bcd18ec4d7f94fd7630
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2021-04-07 16:02:27 +0900

    add repos

---
+++ src/application.ts
@@ -0,0 +1,51 @@
+import {BootMixin} from '@loopback/boot';
+import {ApplicationConfig} from '@loopback/core';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent,
+} from '@loopback/rest-explorer';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {ServiceMixin} from '@loopback/service-proxy';
+import path from 'path';
+import {MySequence} from './sequence';
+import * as dotenv from 'dotenv';
+import * as dotenvExt from 'dotenv-extended';
+
+export {ApplicationConfig};
+
+export class Lb4MultiTenantApplication extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    // Set up the environment
+    dotenv.config();
+    dotenvExt.load({
+      schema: '.env.examples',
+      errorOnMissing: true,
+    });        
+    super(options);
+    // Set up the custom sequence
+    this.sequence(MySequence);
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
+
+    this.projectRoot = __dirname;
+    // Customize @loopback/boot Booter Conventions here
+    this.bootOptions = {
+      controllers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+    };
+  }
+}
---
+++ src/datasources/pgdb.datasource.ts
@@ -0,0 +1,42 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+import * as config from './pgdb.datasource.json';
+/*
+const config = {
+  name: 'pgdb',
+  connector: 'postgresql',
+  url: '',
+  host: '172.30.13.71',
+  port: 5432,
+  user: 'postgres',
+  password: 'postgres',
+  database: 'lbstarter'
+};
+*/
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class PgdbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'pgdb';
+  //static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.pgdb', {optional: true})
+    dsConfig: object = {...config },
+  ) {
+
+    // Override data source config from environment variables
+    Object.assign(dsConfig, {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      schema: process.env.DB_SCHEMA,
+    });
+    super(dsConfig);    
+  }
+}
